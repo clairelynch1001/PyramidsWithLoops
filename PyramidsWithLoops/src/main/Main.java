6c984714d9c215778add2d03c023fd2490cc97f1
lynchcc <lynchcc@LCBLAB2110-13.uccob.uc.edu>
2019-09-24 10:16:56 -0400

    Final Update (9/24/19)

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,7 +1,11 @@
 /* 
  * Claire Lynch
  * lynchcc@ucmail.uc.edu
- * Printing pyramids using loops. It kind-of works. 
+ * Assignment 04
+ * Due Date: 9/26/19
+ * Course: IS4010
+ * Sources: https://www.programiz.com/java-programming/examples/pyramid-pattern
+ * Description: Printing pyramids using loops. It kind-of works. 
  * What I really want is
         1
        222
@@ -14,12 +18,18 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		int lines = 5;
-		for (int i = 1; i <= lines; i++) {
-			for (int j = 1; j < i + i - 1; j++) {
-				System.out.print(i);
+		for (int i = 1; i <= lines; i++) { // Gives our starting number as 1, not zero
+			for (int j = 0; j < lines - i; j++) {
+				System.out.print(" ");
 			}
-			System.out.println(i);
+			for(int k = 1; k<=i + i - 1; k++) { // Gets the correct increase of numbers.. + 2 for each line
+				System.out.print(i + ""); // Prints the correct pyramid shape
+			}
+			System.out.println(); 
+			
+	        }
 		}
 	}
-}
+
